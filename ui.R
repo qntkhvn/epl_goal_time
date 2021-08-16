412f7417d751f4b2b980a9ea93be6ac393b04abd
Quang Nguyen <ngminhquang.99@gmail.com>
2021-08-16 00:09:54 -0400

    Shiny app deployment

--- ui.R
+++ ui.R
@@ -10,7 +10,11 @@ fluidPage(
     
     theme = shinytheme("flatly"),
     
-    titlePanel("EPL Goal Scoring Time"),
+    br(),
+    
+    br(),
+    
+    titlePanel("⚽ EPL GOAL SCORING TIME"),
     
     sidebarLayout(
         
@@ -20,16 +24,18 @@ fluidPage(
               from the inaugural season in 1992–93, to 2020-21, the most recently completed season."),
             
             HTML("<p>The data were obtained from <a href='https://www.transfermarkt.us/premier-league/spieltag/wettbewerb/GB1'>transfermarkt</a>,
-                 and the scraping script is available on <a href='https://github.com/qntkhvn'>GitHub</a>.
+                 and the scraping script is available on <a href='https://github.com/qntkhvn/epl_goal_time/blob/main/goals_scraper.R'>GitHub</a>.
                  In this dataset, the most important feature is the scoring time (in minute) of each goal. As improbable as it may seem, 
-                 
-                 this piece of information does not come in a nicely clean, publicly data format. </p>"),
+                 it is almost impossible (at least for me) to find nicely clean and publicly available data on this specific piece of information.
+                 (If there's a resource (web, package, database, etc.) out there on football goal scoring time data that I'm not aware of, please let me know.)</p>"),
             
             HTML("<p>(A personal story: When I was working on my <a href='https://bookdown.org/theqdata/honors_thesis'>undergraduate honors thesis</a> on modeling EPL goal scoring, 
-                 due to the amount of time and most importantly data science skills I had back then, I couldn't either find an dataset on scoring time, or
-                 scrape the extremely messy data from the web, so I ended up collecting the data by hand. Thus, this is my motivation behind this app.)</p>"),
+                 due to the amount of time and most importantly data science skills I had back then, I couldn't either find a dataset on scoring time, or
+                 scrape the extremely messy data from the web, so I ended up collecting the data by hand. Thus, this is my motivation for developing this app.)</p>"),
             
-            p("Download the full data here"),
+            br(),
+            
+            p("Download the full data"),
             
             downloadButton("downloadData", ".csv")
         ),
